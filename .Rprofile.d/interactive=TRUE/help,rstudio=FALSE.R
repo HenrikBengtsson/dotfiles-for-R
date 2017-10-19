a631901f82d8aba3106ee3fe06ad0b54306cf0b3
hb <hb@aroma-project.org>
2017-10-18 20:18:43 -0700

    Suppress message on HTML help server being started

--- .Rprofile.d/interactive=TRUE/help,rstudio=FALSE.R
+++ .Rprofile.d/interactive=TRUE/help,rstudio=FALSE.R
@@ -1,10 +1,11 @@
-# Setup built-in HTTP daemon
-# Always only the HTML help on the same port
+## Setup built-in HTTP daemon
+## Always only the HTML help on the same port
 local({
   port <- sum(c(1e4, 100) * as.double(R.version[c("major", "minor")]))
   options(help.ports = port + 0:9)
 })
-# Try to start HTML help server
-try(tools::startDynamicHelp())
+
+## Try to start HTML help server
+suppressMessages(try(tools::startDynamicHelp()))
 
 options(help_type = "html")
