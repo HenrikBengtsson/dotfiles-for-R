d0247207f4290495eb7e2fc1d86d004357b44d24
Henrik Bengtsson <hb@aroma-project.org>
2019-03-29 12:48:02 -0700

    More UI settings

---
+++ .Rprofile.d/interactive=TRUE/ui.R
@@ -0,0 +1,8 @@
+## TAB completions
+utils::rc.settings(ipck = TRUE)
+
+## If set and TRUE, R run in a terminal using a recent readline library
+## will set the width option when the terminal is resized.
+options(setWidthOnResize = TRUE)
+
+gui_dialogs("off")
