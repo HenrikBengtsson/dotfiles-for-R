64dcb9d1685a8e158894aa457f55334244e0d2d8
Henrik Bengtsson <hb@aroma-project.org>
2019-09-04 08:26:17 -0700

    ROBUSTNESS: Only call utils::recover() in interactive() mode

--- .Rprofile.d/interactive=TRUE/on_error,ess=FALSE,rstudio=FALSE.R
+++ .Rprofile.d/interactive=TRUE/on_error,ess=FALSE,rstudio=FALSE.R
@@ -2,5 +2,5 @@ options(error = function() {
   ## Close any open 'stdout' sinks including
   ## any active capture.output()
   replicate(sink.number(), sink(NULL))
-  utils::recover()
+  if (interactive()) utils::recover()
 })
