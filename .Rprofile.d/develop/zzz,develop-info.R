3f3636bb5cc6529db96e948e9307c162debe5ebd
Henrik Bengtsson <hb@aroma-project.org>
2019-09-17 11:18:19 -0700

    cleanup

--- .Rprofile.d/develop/zzz,develop-info.R
+++ .Rprofile.d/develop/zzz,develop-info.R
@@ -1,21 +1,28 @@
 ## https://github.com/HenrikBengtsson/r-ideas/issues/81
 local({
+  cmdArgs <- commandArgs()
+  # Skip?
+  if (length(cmdArgs) > 1L) return(invisible())
+  
   file <- "DESCRIPTION"
-  if (utils::file_test("-f", file)) {
-    dcf <- read.dcf(file = file)
-    dcf <- as.data.frame(dcf, stringsAsFactors = FALSE)
-    pkg <- dcf$Package
-    if (length(pkg) == 1L && !is.na(pkg) && nzchar(pkg)) {
-      pkg_ver <- dcf$Version
-      desc <- suppressWarnings(utils::packageDescription(pkg))
-      ver <- NA_character_
-      if (is.list(desc)) {
-        ver <- sprintf("%s (%s)", desc$Version,
-                       dirname(dirname(dirname(attr(desc, "file")))))
-      }
-      url <- dcf$URL
-      if (is.null(url)) url <- NA_character_
-      message(sprintf(
+  if (!utils::file_test("-f", file)) return(invisible())
+
+  dcf <- read.dcf(file = file)
+  dcf <- as.data.frame(dcf, stringsAsFactors = FALSE)
+  pkg <- dcf$Package
+  
+  if (length(pkg) != 1L || is.na(pkg) || !nzchar(pkg)) return(invisible())
+  
+  pkg_ver <- dcf$Version
+  desc <- suppressWarnings(utils::packageDescription(pkg))
+  ver <- NA_character_
+  if (is.list(desc)) {
+    ver <- sprintf("%s (%s)", desc$Version,
+                   dirname(dirname(dirname(attr(desc, "file")))))
+  }
+  url <- dcf$URL
+  if (is.null(url)) url <- NA_character_
+  fmtstr <- {
 "Package folder:\
  - package: %s\
  - devel version: %s\
@@ -27,17 +34,19 @@ local({
  - repos: %s\
  - R check env vars: %s\
  - CRANCACHE_DIR: %s\
- - PWD: %s\n",
-        pkg, pkg_ver, ver,
-	url,
-	getRversion(), R.home(),
-	Sys.getenv("R_LIBS_USER"),
-	paste(sQuote(.libPaths()), collapse=", "),
-	{ repos <- getOption("repos"); paste(sprintf("%s=%s", names(repos), sQuote(repos)), collapse=", ") },
-	{ envs <- Sys.getenv(); envs <- envs[grep("^_?R_CHECK", names(envs))]; envs <- sprintf("%s=%s", names(envs), envs); envs <- paste(envs, collapse=", "); envs },
-	Sys.getenv("CRANCACHE_DIR"),
-	getwd()
-      ))
-    }
+ - PWD: %s\
+ - commandArgs(): %s\n"
   }
+  message(sprintf(fmtstr,   
+    pkg, pkg_ver, ver,
+    url,
+    getRversion(), R.home(),
+    Sys.getenv("R_LIBS_USER"),
+    paste(sQuote(.libPaths()), collapse=", "),
+    { repos <- getOption("repos"); paste(sprintf("%s=%s", names(repos), sQuote(repos)), collapse=", ") },
+    { envs <- Sys.getenv(); envs <- envs[grep("(^_?R_CHECK)", names(envs))]; envs <- sprintf("%s=%s", names(envs), envs); envs <- paste(envs, collapse=", "); envs },
+    Sys.getenv("CRANCACHE_DIR"),
+    getwd(),
+    paste(sQuote(cmdArgs), collapse = " ")
+  ))
 })
