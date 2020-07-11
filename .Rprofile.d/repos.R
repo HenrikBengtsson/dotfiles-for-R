a2f25247095ce8c0d517d99336c08712e9595ba4
Henrik Bengtsson <hb@aroma-project.org>
2020-07-11 11:51:01 -0700

    Don't set 'repos' when 'R CMD ...' run

--- .Rprofile.d/repos.R
+++ .Rprofile.d/repos.R
@@ -8,99 +8,103 @@
 #'
 #' @author Henrik Bengtsson
 #' @imports utils BiocVersion BiocManager startup
-local({
-  known_repos <- function() {
-    p <- file.path(Sys.getenv("HOME"), ".R", "repositories")
-    if (!file.exists(p)) p <- file.path(R.home("etc"), "repositories")
-    ns <- getNamespace("tools")
-    .read_repositories <- get(".read_repositories", envir = ns)
-    a <- .read_repositories(p)
-    repos <- a$URL
-    names(repos) <- rownames(a)
-    repos
-  }
 
-  ## Bioconductor version
-  bioc_version <- function() {
-    biocver <- Sys.getenv("R_BIOC_VERSION")
-    if (nzchar(biocver)) return(biocver)
-    
-    ## Via the BiocVersion package?
-    tryCatch({
-      biocver <- as.character(utils::packageVersion("BiocVersion")[, 1:2])
-    }, error = identity)
-    if (nzchar(biocver)) return(biocver)
-
-    # Via the BiocManager package?
-    ## WORKAROUND: BiocManager::version() can be very slow
-    ## because it calls installed.packages().
-    ## https://github.com/Bioconductor/BiocManager/pull/42
-    tryCatch({
-      biocver <- as.character(BiocManager:::.version_choose_best())
-      unloadNamespace("BiocManager")
-    }, error = identity)
-    if (nzchar(biocver)) return(biocver)
-
-    # Ad hoc via the R version
-    rver <- getRversion()
-    biocver <- {
-      if (rver >= "4.0.0") "3.11" else ## per 2019-10-30
-      if (rver >= "3.6.1") "3.10" else ## per 2019-10-30
-      if (rver >= "3.6.0") "3.9" else
-      if (rver >= "3.5.1") "3.8" else
-      if (rver >= "3.5.0") "3.7" else
-      if (rver >= "3.4.2") "3.6" else
-      if (rver >= "3.4.0") "3.5" else
-      if (rver >= "3.3.1") "3.4" else
-      if (rver >= "3.3.0") "3.3" else
-      if (rver >= "3.2.2") "3.2" else
-      if (rver >= "3.2.0") "3.1" else
-                           "3.0"
+if (!nzchar(Sys.getenv("R_CMD"))) {
+  local({
+    known_repos <- function() {
+      p <- file.path(Sys.getenv("HOME"), ".R", "repositories")
+      if (!file.exists(p)) p <- file.path(R.home("etc"), "repositories")
+      ns <- getNamespace("tools")
+      .read_repositories <- get(".read_repositories", envir = ns)
+      a <- .read_repositories(p)
+      repos <- a$URL
+      names(repos) <- rownames(a)
+      repos
     }
-  }
-
-  Sys.setenv(R_BIOC_VERSION = bioc_version())
-  biocver <- package_version(Sys.getenv("R_BIOC_VERSION"))
-
-  repos <- c(
-    getOption("repos"),
-    known_repos(),
-    "CRAN"       = "https://cloud.r-project.org",
-    "CRANextra"  = if (.Platform$OS.type == "windows") {
-                     "https://www.stats.ox.ac.uk/pub/RWin"
-                   },
-    "R-Forge"    = "http://R-Forge.R-project.org",
-    "Omegahat"   = "http://www.omegahat.org/R",
-    "rforge.net" = "https://www.rforge.net"
-  )
-
-  # Drop remaining '@...@' values
-  repos <- grep("^@.*@$", repos, invert=TRUE, value=TRUE)
-
-  # Drop miscellaneous 
-  repos <- repos[!grepl("(CRANextra|Omegahat|R-Forge|rforge.net)", names(repos))]
-
-  # Bioconductor tweaks
-  if (biocver >= "3.6") {
-    repos <- repos[!grepl("BioCextra", names(repos))]
-  }
-  if (biocver >= "3.7") {
-    repos["BioCworkflows"] <- gsub("bioc$", "workflows", repos[["BioCsoft"]])
-  }
-
-  # Bring CRAN to the front
-  idx <- match("CRAN", names(repos))
-  if (!is.na(idx)) repos <- c(repos[idx], repos[-idx])
   
-  # Use HTTP when HTTPS is not supported
-  if (getRversion() < "3.2.2" || startup::sysinfo()$wine) {
-    repos <- gsub("https://", "http://", repos, fixed = TRUE)
-  }
+    ## Bioconductor version
+    bioc_version <- function() {
+      biocver <- Sys.getenv("R_BIOC_VERSION")
+      if (nzchar(biocver)) return(biocver)
+      
+      ## Via the BiocVersion package?
+      tryCatch({
+        biocver <- as.character(utils::packageVersion("BiocVersion")[, 1:2])
+      }, error = identity)
+      if (nzchar(biocver)) return(biocver)
+  
+      # Via the BiocManager package?
+      ## WORKAROUND: BiocManager::version() can be very slow
+      ## because it calls installed.packages().
+      ## https://github.com/Bioconductor/BiocManager/pull/42
+      tryCatch({
+        biocver <- as.character(BiocManager:::.version_choose_best())
+        unloadNamespace("BiocManager")
+      }, error = identity)
+      if (nzchar(biocver)) return(biocver)
+  
+      # Ad hoc via the R version
+      rver <- getRversion()
+      biocver <- {
+        if (rver >= "4.0.0") "3.11" else ## per 2019-10-30
+        if (rver >= "3.6.1") "3.10" else ## per 2019-10-30
+        if (rver >= "3.6.0") "3.9" else
+        if (rver >= "3.5.1") "3.8" else
+        if (rver >= "3.5.0") "3.7" else
+        if (rver >= "3.4.2") "3.6" else
+        if (rver >= "3.4.0") "3.5" else
+        if (rver >= "3.3.1") "3.4" else
+        if (rver >= "3.3.0") "3.3" else
+        if (rver >= "3.2.2") "3.2" else
+        if (rver >= "3.2.0") "3.1" else
+                             "3.0"
+      }
+    }
+  
+    Sys.setenv(R_BIOC_VERSION = bioc_version())
+    biocver <- package_version(Sys.getenv("R_BIOC_VERSION"))
+  
+    repos <- c(
+      getOption("repos"),
+      known_repos(),
+      "CRAN"       = "https://cloud.r-project.org",
+      "CRANextra"  = if (.Platform$OS.type == "windows") {
+                       "https://www.stats.ox.ac.uk/pub/RWin"
+                     },
+      "R-Forge"    = "http://R-Forge.R-project.org",
+      "Omegahat"   = "http://www.omegahat.org/R",
+      "rforge.net" = "https://www.rforge.net"
+    )
+  
+    # Drop remaining '@...@' values
+    repos <- grep("^@.*@$", repos, invert=TRUE, value=TRUE)
+  
+    # Drop miscellaneous 
+    repos <- repos[!grepl("(CRANextra|Omegahat|R-Forge|rforge.net)", names(repos))]
+  
+    # Bioconductor tweaks
+    if (biocver >= "3.6") {
+      repos <- repos[!grepl("BioCextra", names(repos))]
+    }
+    if (biocver >= "3.7") {
+      repos["BioCworkflows"] <- gsub("bioc$", "workflows", repos[["BioCsoft"]])
+    }
+  
+    # Bring CRAN to the front
+    idx <- match("CRAN", names(repos))
+    if (!is.na(idx)) repos <- c(repos[idx], repos[-idx])
+    
+    # Use HTTP when HTTPS is not supported
+    if (getRversion() < "3.2.2" || startup::sysinfo()$wine) {
+      repos <- gsub("https://", "http://", repos, fixed = TRUE)
+    }
+  
+    # Keep only unique existing ones
+    repos <- repos[!is.na(repos)]
+    names <- names(repos)
+    repos <- repos[!(nzchar(names) & duplicated(names))]
+  
+    options(repos = repos)
+  })
+}
 
-  # Keep only unique existing ones
-  repos <- repos[!is.na(repos)]
-  names <- names(repos)
-  repos <- repos[!(nzchar(names) & duplicated(names))]
-
-  options(repos = repos)
-})
