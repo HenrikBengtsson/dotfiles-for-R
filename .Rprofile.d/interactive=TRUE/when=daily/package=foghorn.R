f67ee5c7bb89ba74406b52c09e25fb1fe045f377
Henrik Bengtsson <hb@aroma-project.org>
2020-10-10 16:38:22 -0700

    CLEANUP: No need for with_nonstrict() with foghorn (>= 1.1.3)

--- .Rprofile.d/interactive=TRUE/when=daily/package=foghorn.R
+++ .Rprofile.d/interactive=TRUE/when=daily/package=foghorn.R
@@ -12,7 +12,7 @@
 try(local({
   email <- Sys.getenv("MY_CRAN_EMAIL", Sys.getenv("MY_EMAIL"))
   if (nzchar(email)) {
-    with_nonstrict(foghorn::summary_cran_results(email))
+    foghorn::summary_cran_results(email)
     unloadNamespace("foghorn")
   }
 }), silent = TRUE)
