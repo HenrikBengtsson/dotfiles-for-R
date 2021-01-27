e64581754b9b24d06ca73b7ec2d63fc2367331a6
Henrik Bengtsson <hb@aroma-project.org>
2021-01-27 10:28:01 -0800

    BUG FIX: rappdirs 0.3.2 broke setup of 'CRANCACHE_DIR' for crancache

--- .Rprofile.d/develop/package=crancache.R
+++ .Rprofile.d/develop/package=crancache.R
@@ -7,6 +7,9 @@ local({
     ## Alt 2. Customized R_LIBS_USER
     lib <- Sys.getenv("R_LIBS_USER")
     version <- c(basename(dirname(lib)), basename(lib))
+
+    ## WORKAROUND: https://github.com/r-lib/rappdirs/issues/32
+    version <- do.call(file.path, args = as.list(version))
     
     rappdirs::user_cache_dir("R-crancache", version = version, expand = TRUE)
   }			  
