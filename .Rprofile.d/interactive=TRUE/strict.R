dc06c60b998a47189469c3cba657a4d538864713
Henrik Bengtsson <hb@aroma-project.org>
2022-01-18 12:46:43 -0800

    Show traceback for warnings by default (showWarnCalls = TRUE)

--- .Rprofile.d/interactive=TRUE/strict.R
+++ .Rprofile.d/interactive=TRUE/strict.R
@@ -4,14 +4,15 @@
 #' * `warnPartialMatchDollar`
 #' * `warnPartialMatchArgs`
 #' * `warnPartialMatchAttr`
+#' * `showWarnCalls`
 #'
 #' @author Henrik Bengtsson
 options(
   warnPartialMatchDollar = TRUE,
-  warnPartialMatchArgs = TRUE,
-  warnPartialMatchAttr = TRUE
+  warnPartialMatchArgs   = TRUE,
+  warnPartialMatchAttr   = TRUE,
+  showWarnCalls          = TRUE  ## show call stack for warnings
 )
-
 
 ## Source: 000.utils/track_rng_on_load.R
 track_rng_on_load("on")
