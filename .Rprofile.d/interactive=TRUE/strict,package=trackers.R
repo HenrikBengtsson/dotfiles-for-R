44c0a9954a2577cadd2fa6827eb64adbbc59ff16
Henrik Bengtsson <hb@aroma-project.org>
2022-11-11 16:57:01 -0800

    tracker_globals() -> tracker_globalenv()

--- .Rprofile.d/interactive=TRUE/strict,package=trackers.R
+++ .Rprofile.d/interactive=TRUE/strict,package=trackers.R
@@ -1,15 +1,15 @@
 invisible({
   library(trackers)
-  addTaskCallback(tracker_envvars,  name = "Environment-variable tracker")
-  addTaskCallback(tracker_globals,  name = "Files tracker")
-  addTaskCallback(tracker_globals,  name = ".GlobalEnv tracker")
-  addTaskCallback(tracker_options,  name = "Options tracker")
-  tracker_locale(NULL) ## initiate  locale tracker
-  addTaskCallback(tracker_locale,   name = "Locale tracker")
-  addTaskCallback(tracker_packages, name = "Packages tracker")
-  addTaskCallback(tracker_rng,      name = "RNG tracker")
-  addTaskCallback(tracker_rplots,   name = "Rplots tracker")
-  addTaskCallback(tracker_sink,     name = "Sink tracker")
+  addTaskCallback(tracker_envvars,   name = "Environment-variable tracker")
+  addTaskCallback(tracker_files,     name = "Files tracker")
+  addTaskCallback(tracker_globalenv, name = ".GlobalEnv tracker")
+  addTaskCallback(tracker_options,   name = "Options tracker")
+  tracker_locale(NULL) ## initiate locale tracker
+  addTaskCallback(tracker_locale,    name = "Locale tracker")
+  addTaskCallback(tracker_packages,  name = "Packages tracker")
+  addTaskCallback(tracker_rng,       name = "RNG tracker")
+  addTaskCallback(tracker_rplots,    name = "Rplots tracker")
+  addTaskCallback(tracker_sink,      name = "Sink tracker")
 })
 
 trackers::trace_rng_on_load("on")
