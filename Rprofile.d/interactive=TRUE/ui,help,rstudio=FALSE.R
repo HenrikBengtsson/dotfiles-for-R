ed79f2765e7c4b5f70a3c7d070b5d62e8e965362
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-05-25 09:23:01 -0700

    Make option 'browser' open local HTML files in Chrome

--- Rprofile.d/interactive=TRUE/ui,help,rstudio=FALSE.R
+++ Rprofile.d/interactive=TRUE/ui,help,rstudio=FALSE.R
@@ -21,4 +21,21 @@ suppressMessages(try(tools::startDynamicHelp()))
 
 options(help_type = "html")
 
-if (nzchar(Sys.which("xdg-open"))) options(browser="xdg-open")
+
+## Open file:// in Chrome, everything else in system default browser
+options(browser = function(url) {
+  is_uri <- grepl("^[[:alpha:]]+://", url, ignore.case = TRUE)
+  is_file_uri <- grepl("^file://", url, ignore.case = TRUE)
+  is_html_file <- grepl("[.](htm|html)$", url, ignore.case = TRUE)
+
+  ## Special case: Firefox does not allow to open local HTML files
+  if (is_html_file && (!is_url || is_file_uri)) {
+    bin <- Sys.which("google-chrome")
+    if (!nzchar(bin)) stop("Please install google-chrome")
+  } else {
+    bin <- Sys.which("xdg-open")
+    if (!nzchar(bin)) stop("Please install xdg-open")
+  }
+  browseURL(url, browser = bin)
+})
+
