e856f642f91eabc6fb69b1df39762ee9e5ee2159
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-09-22 09:01:06 -0700

    session stats: make time units agile, e.g. secs, mins, and hours

--- Rprofile.d/interactive=TRUE/session_stats.R
+++ Rprofile.d/interactive=TRUE/session_stats.R
@@ -5,20 +5,40 @@ if (interactive() && requireNamespace("startup", quietly = TRUE)) {
   gc.time(on = TRUE)
   startup::on_session_exit(local({
     t0 <- Sys.time()
+
+    difftime2txt <- function(x) {
+      txt <- structure(character(length(x)), names = names(x))
+      for (name in names(x)) {
+        dt <- x[[name]]
+        unit <- "secs"
+        if (dt >= 3600L) {
+          unit <- "hours"
+          dt <- dt / 3600L
+        } else if (dt >= 60L) {
+          unit <- "mins"
+          dt <- dt / 60L
+        }
+        txt[name] <- sprintf("%.2f %s", dt, unit)
+      }
+      txt
+    }
+    
     function(...) {
-      dt <- difftime(Sys.time(), t0, units = "auto")
-      dt_p <- proc.time()
-      names <- c("user", "system", "ellapsed", "user_children", "system_children")
-      names(dt_p) <- names
-      dt_gc <- structure(gc.time(), class = "difftime", units = "secs", names = names)
+      dt_w <- difftime(Sys.time(), t0, units = "auto")
+      dt_p <- structure(proc.time(), class = "difftime", units = "secs", names = c("user", "system", "ellapsed", "user_children", "system_children"))
+      dt_gc <- structure(gc.time(), class = "difftime", units = "secs", names = names(dt_p))
+
+      dt_p_txt <- difftime2txt(dt_p)
+      dt_gc_txt <- difftime2txt(dt_gc)
+
       msg <- c(
         "Session summary:",
         sprintf(" * R version: %s", getRversion()),
         sprintf(" * Hostname: %s", Sys.info()[["nodename"]]),
         sprintf(" * Process ID: %d", Sys.getpid()),
-        sprintf(" * Garbage collection time: %s (%s)", paste(sprintf("%s=%.2f (%.1f%%)", names(dt_gc), dt_gc, 100 * dt_gc / dt_p), collapse = ", "), attr(dt_gc, "units")),
-        sprintf(" * Processing time: %s (secs)", paste(sprintf("%s=%.2f", names(dt_p), dt_p), collapse = ", ")),
-        sprintf(" * Wall time: %.2f %s", dt, attr(dt, "units"))
+        sprintf(" * Garbage collection time: %s", paste(sprintf("%s=%s (%.1f%%)", names(dt_gc_txt), dt_gc_txt, 100 * dt_gc / as.numeric(dt_p)), collapse = ", ")),
+        sprintf(" * Processing time: %s", paste(sprintf("%s=%s (%.1f%%)", names(dt_p_txt), dt_p_txt, 100 * as.numeric(dt_p) / as.numeric(dt_p[["ellapsed"]])), collapse = ", ")),
+        sprintf(" * Wall time: %.2f %s", dt_w, attr(dt_w, "units"))
       )
       msg <- paste(msg, collapse = "\n")
       message(msg)
