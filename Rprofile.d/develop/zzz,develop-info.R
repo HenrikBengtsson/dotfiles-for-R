48206440052eb4730880bbec96a98c861e31eb4d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2024-11-11 17:28:20 -0800

    Package folder: report on GitHub Action files + scan for outdated action versions

--- Rprofile.d/develop/zzz,develop-info.R
+++ Rprofile.d/develop/zzz,develop-info.R
@@ -5,7 +5,7 @@
 local({
   cmdArgs <- commandArgs()
   # Skip?
-  if (length(cmdArgs) > 1L) return(invisible())
+  if (!interactive() || length(cmdArgs) > 1L) return(invisible())
   
   file <- "DESCRIPTION"
   if (!utils::file_test("-f", file)) return(invisible())
@@ -38,8 +38,14 @@ local({
  - R check env vars: %s\
  - CRANCACHE_DIR: %s\
  - PWD: %s\
- - commandArgs(): %s\n"
+ - commandArgs(): %s\
+ - .github/workflows/: %s\n"
   }
+
+  gha_files <- if (utils::file_test("-d", ".github/workflows")) {
+    dir(path = ".github/workflows", pattern = "[.](yml|yaml)$", full.names = TRUE)
+  }
+
   message(sprintf(fmtstr,   
     pkg, pkg_ver, ver,
     url,
@@ -50,9 +56,43 @@ local({
     { envs <- Sys.getenv(); envs <- envs[grep("(^_?R_CHECK)", names(envs))]; envs <- sprintf("%s=%s", names(envs), envs); envs <- paste(envs, collapse=", "); envs },
     Sys.getenv("CRANCACHE_DIR"),
     getwd(),
-    paste(sQuote(cmdArgs), collapse = " ")
+    paste(sQuote(cmdArgs), collapse = " "),
+    paste(basename(gha_files), collapse = ", ")
   ))
-  
+
+  actions <- c(
+    ## GitHub
+    "actions/checkout@v4",
+    "actions/upload-artifact@v4",
+    ## r-lib
+    "r-lib/actions/setup-pandoc@v2",
+    "r-lib/actions/setup-r@v2",
+    "r-lib/actions/setup-r-dependencies@v2",
+    ## r-hub
+    "r-hub/actions/setup@v1",
+    "r-hub/actions/checkout@v1",
+    "r-hub/actions/setup-r@v1"
+  )
+  ## Validate GitHub Action files
+  for (file in gha_files) {
+    old <- NULL
+    bfr <- readLines(file, warn = FALSE)
+    for (action in actions) {
+      version <- sub(".*@", "", action)
+      action <- sub("@.*", "", action)
+      pattern <- sprintf(".*[[:space:]](%s)@(v[[:digit:]]+).*", action)
+      lines <- grep(pattern, bfr, value = TRUE)
+      if (length(lines) > 0) {
+        v <- sub(pattern, "\\2", lines)
+        v <- v[(v != version)]
+        if (length(v) > 0) old <- c(old, sprintf("%s@%s", action, v))
+      }
+    }
+    if (length(old) > 0) {
+      warning(sprintf("%s uses outdated actions: [n=%d] %s\n", file, length(old), paste(old, collapse = ", ")), immediate. = TRUE, call. = FALSE)
+    }
+  }
+
   ## For conviency:
   assign(".packageName", pkg, envir = globalenv())
 })
