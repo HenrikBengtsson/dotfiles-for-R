8a48c810c23a1a622843483bdd184a9da501069c
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-20 20:05:34 -0700

    Show longer call stacks

---
+++ Rprofile.d/options.R
@@ -0,0 +1,5 @@
+# Controls how long the sequence of calls must be (in bytes) before
+# ellipses are used. Defaults to 50 and should be at least 30 and no
+# more than 500.
+options(showNCalls = 500L)
+
