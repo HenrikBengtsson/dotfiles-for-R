59d35f4a207c85c0ae2cf105ab38e8eef22e3197
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-08-11 16:00:22 -0400

    TIL: Lionel Henry showed that stdout() != getConnection(1) when sink:ed

--- Rprofile.d/interactive=TRUE/supersink.R
+++ Rprofile.d/interactive=TRUE/supersink.R
@@ -1,19 +1,27 @@
-#' Sink standard output to standard error in browser
-#'
-#' EXAMPLE:
-#' > foo <- function() browser()
-#' > utils::capture.output(foo())
-#' Browse[1]> 1+2
-#' Browse[1]> supersink()
-#' Browse[1]> 1+2
-#' [1] 3
-#' Browse[1]> c
-#' [1] "Called from: browser()" "[1] 3"
+#' Circumvent standard output being sink:ed when debugging with browser
+#' 
+#' @examples interactive()
+#' test <- function() browser()
+#' out <- utils::capture.output(test())
+#' #> Browse[1]> 1+2
+#' #> Browse[1]> supersink()
+#' #> Browse[1]> 1+2
+#' #> [1] 3
+#' #> Browse[1]> c
 #' > 
 #' 
-#' REFERENCES:
-#' * https://github.com/HenrikBengtsson/Wishlist-for-R/issues/90
-supersink <- function(envir = parent.frame()) {
-  sink(stderr(), split = TRUE)
-  do.call(base::on.exit, args = list(expr = quote(sink()), add = TRUE, after = TRUE), envir = envir)
+#' @authors
+#' Henrik Bengtsson, Lionel Henry, Tomasz Kalinowski (R Dev Day; useR! 2025)
+#' 
+#' @references
+#' * <https://github.com/HenrikBengtsson/Wishlist-for-R/issues/90>
+#' 
+#' @export
+supersink <- function() {
+  base::sink(base::getConnection(1), type = "output", split = TRUE)
+  base::do.call(base::on.exit, args = list(
+    expr = base::quote(sink()),
+    add = TRUE,
+    after = TRUE
+  ), envir = base::parent.frame())
 }
